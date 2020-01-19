3f820b9d604c3edd1f5599c9403cb4942b928b5a
rvelazquezjr <51425632+rvelazquezjr@users.noreply.github.com>
2020-01-19 01:44:59 -0500

    Update liri.js

--- Source/liri.js
+++ Source/liri.js
@@ -7,20 +7,19 @@ var spotify = new spotify(keys.SPOTIFY_ID);
 var omdb = new omdb(keys.omdb);
 
 var movieName = process.argv[2];
-var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=[cf5cf626]&";
+var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=&";
 
 console.log(queryURL);
 axios
   .get()
   .then(function(response) {
-    // If the axios was successful...
-    // Then log the body from the site!
+    
+    
     console.log(response.data);
   })
   .catch(function(error) {
     if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
+      
       console.log("---------------Data---------------");
       console.log(error.response.data);
       console.log("---------------Worf---------------");
@@ -28,11 +27,10 @@ axios
       console.log("---------------Shut up, Wesley!!!---------------");
       console.log(error.response.headers);
     } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      
       console.log(error.request);
     } else {
-      // Something happened in setting up the request that triggered an Error
+      
       console.log("Error", error.message);
     }
     console.log(error.config);
