0a8e3c9bc38765c78e75b387e4844a2a8f4046e4
rvelazquezjr <51425632+rvelazquezjr@users.noreply.github.com>
2020-01-19 01:42:38 -0500

    Update key.js

--- Source/key.js
+++ Source/key.js
@@ -1,8 +1,8 @@
-var SPOTIFY_ID = 'ee63d40825824bd2be9bc8f062e7f966'; // Your client id
-var SPOTIFY_SECRET = '28003fa88aab44bfb15ff9b1094baa1f'; // Your secret
-var redirect_uri = 'http://localhost:3000/callback'; // Your redirect uri
+var SPOTIFY_ID = '';
+var SPOTIFY_SECRET = ''
+var redirect_uri = 'http://localhost:3000/callback';
 var scopes = 'user-read-private user-read-email';
-var omdb = 'cf5cf626';
+var omdb = '';
 
 
 
